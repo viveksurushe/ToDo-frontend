bf3b279e750e5d057602231e14057b65fca299ee
vivek surushe <viveksurushe143@gmail.com>
2019-03-03 23:53:20 +0530

    getall,delete,update frontend

--- src/app/single.service.ts
+++ src/app/single.service.ts
@@ -11,6 +11,10 @@ export class SingleService {
   private url = 'http://localhost:4000';
   constructor(public http: HttpClient) { }
 
+  public getAllList(): Observable<any>{
+    return this.http.get(`${this.url}/api/v1/single/getAllList`);
+  }// end of th e signup function
+
   public addToList(data): Observable<any>{
     const params = new HttpParams()
     .set("listName",data)
@@ -18,4 +22,18 @@ export class SingleService {
     console.log(params);
     return this.http.post(`${this.url}/api/v1/single/addlist`,params);
   }// end of th e signup function
+
+  public deleteList(data): Observable<any>{
+    const params = new HttpParams()
+    .set("listId",data)
+    return this.http.post(`${this.url}/api/v1/single/deleteList`,params);
+  }// end of th e deleteList function
+
+  public updateList(data): Observable<any>{
+    const params = new HttpParams()
+    .set("listId",data.listId)
+    .set("listName",data.listName)
+    console.log(params);
+    return this.http.post(`${this.url}/api/v1/single/updatelist`,params);
+  }// end of th e updateList function
 }
--- src/app/single/todolist/todolist.component.ts
+++ src/app/single/todolist/todolist.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SingleService } from 'src/app/single.service';
 import { ToastrService } from 'ngx-toastr';
-
+import { Subject } from 'rxjs';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
@@ -9,11 +10,28 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class TodolistComponent implements OnInit {
 
-  public arr=[
-    {"name":"acd"},
-    {"name":"df"}];
+  public arr=[];
   public inpName:String;
-  constructor(public singleService:SingleService,public toastr: ToastrService,) { }
+  public inpUpdate:string;
+  public listIdHidden:String;
+  refresh: Subject<any> = new Subject();
+  constructor(public singleService:SingleService,public toastr: ToastrService,) {
+    
+      this.singleService.getAllList().subscribe(
+        (apiResponse) => {
+          console.log(apiResponse);
+          if (apiResponse.status === 200) {
+            this.arr=apiResponse.data;
+          } else {
+            this.toastr.error(apiResponse.message);
+          }
+          this.refresh.next();
+        },
+        (err) => {
+          this.toastr.error('Some error occured');
+        }
+      )
+   }
   
   public add: any = () => {
     console.log("work",this.inpName);
@@ -38,8 +56,56 @@ export class TodolistComponent implements OnInit {
     
     
   }
-  public delete:any =()=>{
+  public delete:any =(listId)=>{
     this.arr.pop();
+    console.log("listId",listId);
+    this.singleService.deleteList(listId).subscribe(
+      (apiResponse) => {
+        console.log(apiResponse);
+        if (apiResponse.status === 200) {
+          this.toastr.success("ToDo List Deleted Successfully");
+        } else {
+          this.toastr.error(apiResponse.message);
+        }
+      },
+      (err) => {
+        this.toastr.error('Some error occured');
+      }
+    )
+  }
+
+  public editView:any=(data)=>{
+    console.log(data);
+    $("#insert").hide();
+    $("#update").show();
+    this.inpUpdate=data.listName;
+    this.listIdHidden=data.listId;
+  }
+
+  public update:any=()=>{
+    if(!this.inpUpdate){
+      this.toastr.warning("Enter Edited Field")
+    }else{
+      let upData={
+        listId:this.listIdHidden,
+        listName:this.inpUpdate
+      }
+     this.singleService.updateList(upData).subscribe(
+      (apiResponse) => {
+        console.log(apiResponse);
+        if (apiResponse.status === 200) {
+          this.toastr.success("ToDo List Name Updated");
+          this.listIdHidden=null;
+          this.inpUpdate=null;
+        } else {
+          this.toastr.error(apiResponse.message);
+        }
+      },
+      (err) => {
+        this.toastr.error('Some error occured');
+      }
+    )
+    }
   }
   ngOnInit() {
   }
