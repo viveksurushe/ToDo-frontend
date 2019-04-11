4c2b30d7990238503b99f6456f4aa6e849f1b82e
vivek surushe <viveksurushe143@gmail.com>
2019-04-11 07:55:22 +0530

    socket service added

---
+++ src/app/socket.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Cookie } from 'ng2-cookies/ng2-cookies';
+import * as io from 'socket.io-client'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+
+  private url='http://localhost:4000';
+  private socket;
+  
+  constructor(private http:HttpClient) { 
+
+    this.socket=io(this.url);
+
+  }
+
+  //events to be listened
+
+  public updatedList = () => {
+
+    return Observable.create((observer)=>{
+
+      this.socket.on('updatedList',(data)=>{
+
+        // console.log("-->",data);
+        observer.next(data);
+
+      })//end socket
+
+    })//end observable
+  }
+
+  public updatedFrd = () =>{
+
+    return Observable.create((observer)=>{
+
+      this.socket.on('updatedFrd',(data)=>{
+
+        //console.log("-->",data);
+        observer.next(data);
+
+      })//end socket
+
+    })//end observable
+  }
+
+  // //events to be emited
+  
+  public updateList = () => {
+
+    this.socket.emit('updateList',Cookie.get('userId'));
+
+  }
+
+  public frdlist = () =>{
+
+    this.socket.emit('updateFrd');
+
+  }
+}
